029178799e66c8d0b3a3bf20b1c624d63b0ac31d
brijmohan17 <brijmohangour895@gmail.com>
2025-09-02 01:06:15 +0530

    chnage code

--- Main.java
+++ Main.java
@@ -6,22 +6,22 @@ public class Main {
         System.out.println("Hello, World!");
 
         // Create a Scanner object to read input from the user
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        // Ask the user for two numbers
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
+        // // Ask the user for two numbers
+        // System.out.print("Enter first number: ");
+        // int num1 = scanner.nextInt();
 
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
+        // System.out.print("Enter second number: ");
+        // int num2 = scanner.nextInt();
 
-        // Perform a simple addition
-        int sum = num1 + num2;
+        // // Perform a simple addition
+        // int sum = num1 + num2;
 
-        // Display the result
-        System.out.println("The sum is: " + sum);
+        // // Display the result
+        // System.out.println("The sum is: " + sum);
 
-        // Close the scanner
-        scanner.close();
+        // // Close the scanner
+        // scanner.close();
     }
 }
